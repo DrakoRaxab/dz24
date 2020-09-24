3351a1a702bdb21a3dd721a2b2e0663e763739a1
DrakoRaxab <legbod@gmail.com>
2020-09-24 23:21:57 +0300

    dz24 v.2

--- src/com/company/Logic.java
+++ src/com/company/Logic.java
@@ -1,4 +1,52 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Logic {
+    protected static double getNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+        if (sc.hasNextDouble()) {
+            return sc.nextDouble();
+        } else {
+            System.out.println("Ошибка при вводе. Повторите ввод");
+            return getNumber();
+        }
+    }
+
+    protected static char getOperation() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите операцию: +, -, * или /");
+        char operation;
+        if (sc.hasNext()) {
+            operation = sc.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            sc.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+
+    protected static double calc(double num1, double num2, char operation) {
+        double result;
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(num1, num2, getOperation());
+        }
+        return result;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,7 @@
 package com.company;
 
-import java.util.Scanner;
 
-public class Main {
+public class Main extends Logic {
 
     public static void main(String[] args) {
         double num1 = getNumber();
@@ -12,54 +11,5 @@ public class Main {
         System.out.println("Результат:" + result);
     }
 
-    public static double getNumber() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число:");
-        if (sc.hasNextDouble()) {
-            return sc.nextDouble();
-        } else {
-            System.out.println("Ошибка при вводе. Повторите ввод");
-            return getNumber();
-        }
-    }
 
-    public static char getOperation() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Выберите номер операции:\n1 - прибавить\n2 - отнять\n3 - умножить\n4 - разделить");
-        int operationNumber;
-        if (sc.hasNextInt()) {
-            operationNumber = sc.nextInt();
-        } else {
-            System.out.println("Вы ввели не число! Повторите ввод!");
-            return getOperation();
-        }
-        switch (operationNumber) {
-            case 1:
-                return '+';
-            case 2:
-                return '-';
-            case 3:
-                return '*';
-            case 4:
-                return '/';
-            default:
-                System.out.println("Неправильная операция! Повторите ввод!");
-                return getOperation();
-        }
-    }
-
-    public static double calc(double num1, double num2, char operation) {
-        switch (operation) {
-            case '+':
-                return num1 + num2;
-            case '-':
-                return num1 - num2;
-            case '/':
-                return num1 / num2;
-            case '*':
-                return num1 * num2;
-            default:
-                return Double.NaN;
-        }
-    }
 }
